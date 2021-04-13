d6cf28078e9ee9465f9d45d44c9a568f3aab39a5
SimonAtelier <simon_atelier@web.de>
2021-04-13 13:34:30 +0200

    Implemented CountDown

---
+++ MiniGameAPI/src/entities/CountDown.java
@@ -0,0 +1,225 @@
+package entities;
+
+public class CountDown {
+
+	private long tickCount;
+	private long secondsToCountDown;
+	private long secondsLeft;
+	private State state;
+
+	public CountDown() {
+		state = new StoppedState();
+	}
+
+	public void tick() {
+		state.tick();
+	}
+
+	public void start() {
+		state.start();
+	}
+
+	public void stop() {
+		state.stop();
+	}
+
+	public void pause() {
+		state.pause();
+	}
+
+	public boolean isStopped() {
+		return state.isStopped();
+	}
+
+	public boolean isPaused() {
+		return state.isPaused();
+	}
+
+	public long getTickCount() {
+		return tickCount;
+	}
+
+	private void setTickCount(long tickCount) {
+		this.tickCount = tickCount;
+	}
+
+	public long getSecondsToCountDown() {
+		return secondsToCountDown;
+	}
+
+	public void setSecondsToCountDown(long secondToCountDown) {
+		this.secondsToCountDown = secondToCountDown;
+		this.secondsLeft = secondToCountDown;
+	}
+
+	public long getSecondsLeft() {
+		return secondsLeft;
+	}
+
+	private void setState(State state) {
+		this.state = state;
+		state.onEnterState();
+	}
+
+	private abstract class State {
+
+		public abstract void tick();
+
+		public abstract void start();
+
+		public abstract void stop();
+
+		public abstract void pause();
+
+		public abstract boolean isStopped();
+
+		public abstract boolean isPaused();
+
+		public abstract void onEnterState();
+
+	}
+
+	private class StoppedState extends State {
+
+		@Override
+		public void tick() {
+		}
+
+		@Override
+		public void start() {
+			setState(new StartedState());
+		}
+
+		@Override
+		public void stop() {
+			throw new AlreadyStoppedException();
+		}
+
+		@Override
+		public void pause() {
+			setState(new PausedState());
+		}
+
+		@Override
+		public boolean isStopped() {
+			return true;
+		}
+
+		@Override
+		public boolean isPaused() {
+			return false;
+		}
+
+		@Override
+		public void onEnterState() {
+			setTickCount(0);
+		}
+
+	}
+
+	private class StartedState extends State {
+
+		@Override
+		public void tick() {
+			tickCount++;
+			if (tickedOneSeconds() && stillSecondsLeft()) {
+				secondsLeft--;
+			}
+
+		}
+
+		private boolean tickedOneSeconds() {
+			return tickCount % 20 == 0;
+		}
+
+		private boolean stillSecondsLeft() {
+			return secondsLeft > 0;
+		}
+
+		@Override
+		public void start() {
+			throw new AlreadyStartedException();
+		}
+
+		@Override
+		public void stop() {
+			setState(new StoppedState());
+		}
+
+		@Override
+		public void pause() {
+			setState(new PausedState());
+		}
+
+		@Override
+		public boolean isStopped() {
+			return false;
+		}
+
+		@Override
+		public boolean isPaused() {
+			return false;
+		}
+
+		@Override
+		public void onEnterState() {
+		}
+
+	}
+
+	private class PausedState extends State {
+
+		@Override
+		public void tick() {
+		}
+
+		@Override
+		public void start() {
+			setState(new StartedState());
+		}
+
+		@Override
+		public void stop() {
+			setState(new StoppedState());
+		}
+
+		@Override
+		public void pause() {
+			throw new AlreadyPausedException();
+		}
+
+		@Override
+		public boolean isStopped() {
+			return false;
+		}
+
+		@Override
+		public boolean isPaused() {
+			return true;
+		}
+
+		@Override
+		public void onEnterState() {
+		}
+
+	}
+
+	public class AlreadyPausedException extends RuntimeException {
+
+		private static final long serialVersionUID = 1L;
+
+	}
+
+	public class AlreadyStartedException extends RuntimeException {
+
+		private static final long serialVersionUID = 1L;
+
+	}
+
+	public class AlreadyStoppedException extends RuntimeException {
+
+		private static final long serialVersionUID = 1L;
+
+	}
+
+}
---
+++ MiniGameAPI/test/tests/CountDownTest.java
@@ -0,0 +1,235 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import entities.CountDown;
+import entities.CountDown.AlreadyPausedException;
+import entities.CountDown.AlreadyStartedException;
+import entities.CountDown.AlreadyStoppedException;
+
+public class CountDownTest {
+	
+	private CountDown countDown;
+	
+	@Before
+	public void setUp() {
+		countDown = new CountDown();
+	}
+	
+	@Test
+	public void isStoppedByDefault() {
+		assertEquals(true, countDown.isStopped());
+	}
+	
+	@Test
+	public void isPausedIsFalseByDefault() {
+		assertEquals(false, countDown.isPaused());
+	}
+	
+	@Test
+	public void isStoppedReturnsFalseAfterStart() {
+		countDown.start();
+		assertEquals(false, countDown.isStopped());
+	}
+	
+	@Test
+	public void stopAfterStartReturnsTrueForIsStopped() {
+		countDown.start();
+		assertEquals(false, countDown.isStopped());
+		countDown.stop();
+		assertEquals(true, countDown.isStopped());
+	}
+	
+	@Test
+	public void getTickCountReturnsZeroByDefault() {
+		long expected = 0;
+		assertEquals(expected, countDown.getTickCount());
+	}
+	
+	@Test
+	public void tickStartedCountDownRandomTimesIncrementsTickCount() {
+		int random = (int) (Math.random() * 1000);
+		countDown.start();
+		for (int i = 0; i < random; i++) {
+			countDown.tick();
+		}
+		assertEquals(random, countDown.getTickCount());
+	}
+	
+	@Test
+	public void tickStoppedCountDownRandomTimesDoesNotIncrementTickCount() {
+		int random = (int) (Math.random() * 500);
+		for (int i = 0; i < random; i++) {
+			countDown.tick();
+		}
+		assertEquals(0, countDown.getTickCount());
+	}
+	
+	@Test
+	public void stopResetsTickCountToZero() {
+		countDown.start();
+		countDown.tick();
+		countDown.stop();
+		assertEquals(0, countDown.getTickCount());
+		countDown.start();
+		countDown.tick();
+		assertEquals(1, countDown.getTickCount());
+	}
+	
+	@Test
+	public void tickPausedCountDownRandomTimesDoesNotIncrementTickCount() {
+		int random = (int) (Math.random() * 345);
+		countDown.start();
+		countDown.pause();
+		for (int i = 0; i < random; i++) {
+			countDown.tick();
+		}
+		assertEquals(0, countDown.getTickCount());
+	}
+	
+	@Test
+	public void pauseDoesNotResetTickCountToZero() {
+		countDown.start();
+		countDown.tick();
+		countDown.pause();
+		assertEquals(1, countDown.getTickCount());
+	}
+	
+	@Test
+	public void isPausedReturnsTrueAfterPause() {
+		countDown.start();
+		countDown.pause();
+		assertEquals(true, countDown.isPaused());
+	}
+	
+	@Test
+	public void stopPaused() {
+		countDown.start();
+		countDown.tick();
+		countDown.pause();
+		countDown.stop();
+		assertEquals(false, countDown.isPaused());
+	}
+	
+	@Test
+	public void secondsToCountDownAreZeroByDefault() {
+		assertEquals(0, countDown.getSecondsToCountDown());
+	}
+	
+	@Test
+	public void setSecondsToCountDown() {
+		int random = (int) (Math.random() * 301);
+		countDown.setSecondsToCountDown(random);
+		assertEquals(random, countDown.getSecondsToCountDown());
+	}
+	
+	@Test
+	public void isNotPausedIfStart() {
+		countDown.start();
+		assertEquals(false, countDown.isPaused());
+	}
+	
+	@Test
+	public void isNotStoppedIfStart() {
+		countDown.start();
+		assertEquals(false, countDown.isStopped());
+	}
+	
+	@Test (expected = AlreadyStartedException.class)
+	public void startTwiceThrowsException() {
+		countDown.start();
+		countDown.start();
+	}
+	
+	@Test (expected = AlreadyStoppedException.class)
+	public void stopWithoutStartThrowsException() {
+		countDown.stop();
+	}
+	
+	@Test (expected = AlreadyPausedException.class)
+	public void pauseTwiceThrowsException() {
+		countDown.pause();
+		countDown.pause();
+	}
+	
+	@Test
+	public void isNotStoppedIfPaused() {
+		countDown.pause();
+		assertEquals(false, countDown.isStopped());
+	}
+		
+	@Test
+	public void fromPausedToStart() {
+		int random = (int) (Math.random() * 400);
+		countDown.pause();
+		countDown.start();
+		for (int i = 0; i < random; i++) {
+			countDown.tick();
+		}
+		assertEquals(random, countDown.getTickCount());
+	}
+	
+	@Test
+	public void resume() {
+		int random = (int) (Math.random() * 441);
+		countDown.start();
+		for (int i = 0; i < random; i++) {
+			countDown.tick();
+		}
+		countDown.pause();
+		countDown.tick();
+		
+		countDown.start();
+		assertEquals(random, countDown.getTickCount());
+	}
+	
+	@Test
+	public void secondsLeftReturnsZeroByDefault() {
+		assertEquals(0, countDown.getSecondsLeft());
+	}
+	
+	@Test
+	public void secondsLeftIsEqualToSecondsToCountDownAfterSet() {
+		int random = (int) (Math.random() * 1000);
+		countDown.setSecondsToCountDown(random);
+		assertEquals(random, countDown.getSecondsLeft());
+	}
+	
+	@Test
+	public void countDownOneSecondWithTwentyTicks() {
+		countDown.setSecondsToCountDown(10);
+		countDown.start();
+		
+		for (int i = 0; i < 20; i++) {
+			countDown.tick();
+		}
+		
+		assertEquals(9, countDown.getSecondsLeft());
+	}
+	
+	@Test
+	public void secondsToCountDownAreNotChangedByTick() {
+		countDown.setSecondsToCountDown(10);
+		
+		for (int i = 0; i < 20; i++) {
+			countDown.tick();
+		}
+		
+		assertEquals(10, countDown.getSecondsToCountDown());
+	}
+	
+	@Test
+	public void countDownMoreSecondsThanSetSecondsLeftIsZero() {
+		int seconds = 10;
+		countDown.setSecondsToCountDown(seconds);
+		countDown.start();
+		for (int i = 0; i < (seconds + 1) * 20; i++) {
+			countDown.tick();
+		}
+		assertEquals(0, countDown.getSecondsLeft());
+	}
+		
+}
