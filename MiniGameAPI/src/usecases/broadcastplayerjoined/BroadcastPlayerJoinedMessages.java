23b9581858dcd421da8ad6c42446c763560a8067
SimonAtelier <simon_atelier@web.de>
2021-04-19 21:25:40 +0200

    Changed the messages to English.

--- MiniGameAPI/src/usecases/broadcastplayerjoined/BroadcastPlayerJoinedMessages.java
+++ MiniGameAPI/src/usecases/broadcastplayerjoined/BroadcastPlayerJoinedMessages.java
@@ -2,12 +2,12 @@ package usecases.broadcastplayerjoined;
 
 public interface BroadcastPlayerJoinedMessages {
 
-	static final String PLAYER_JOINED_GAME_MESSAGE_OTHERS = "Der Spieler '$name$' hat das Spiel betreten.";
+	static final String PLAYER_JOINED_GAME_MESSAGE_OTHERS = "'$name$' has joined the game.";
 	
-	static final String PLAYER_JOINED_GAME_MESSAGE_SELF = "Du hast das Spiel betreten.";
+	static final String PLAYER_JOINED_GAME_MESSAGE_SELF = "You joined the game.";
 	
-	static final String PLAYER_JOINED_NO_SUCH_GAME_MESSAGE = "Es existiert kein Spiel mit dem Namen '$name$'.";
+	static final String PLAYER_JOINED_NO_SUCH_GAME_MESSAGE = "The game '$name$' does not exist.";
 	
-	static final String PLAYER_JOINED_GAME_JOINED_PLAYER_ID_IS_INVALID = "Ungültige Spieler-Id.";
+	static final String PLAYER_JOINED_GAME_JOINED_PLAYER_ID_IS_INVALID = "Invalid player-id.";
 	
 }
