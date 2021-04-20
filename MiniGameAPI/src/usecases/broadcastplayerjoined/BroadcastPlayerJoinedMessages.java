185325b233f4158ada873995d85f994d26c68a5e
SimonAtelier <simon_atelier@web.de>
2021-04-20 08:58:38 +0200

    Changed messages.

--- MiniGameAPI/src/usecases/broadcastplayerjoined/BroadcastPlayerJoinedMessages.java
+++ MiniGameAPI/src/usecases/broadcastplayerjoined/BroadcastPlayerJoinedMessages.java
@@ -6,8 +6,8 @@ public interface BroadcastPlayerJoinedMessages {
 	
 	static final String PLAYER_JOINED_GAME_MESSAGE_SELF = "You joined the game.";
 	
-	static final String PLAYER_JOINED_NO_SUCH_GAME_MESSAGE = "The game '$name$' does not exist.";
+	static final String PLAYER_JOINED_NO_SUCH_GAME_MESSAGE = "An internal error occured: The game '$name$' does not exist.";
 	
-	static final String PLAYER_JOINED_GAME_JOINED_PLAYER_ID_IS_INVALID = "Invalid player-id.";
+	static final String PLAYER_JOINED_GAME_JOINED_PLAYER_ID_IS_INVALID = "An internal error occured: Invalid player-id.";
 	
 }
