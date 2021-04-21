6b57ec1472f7e9d24a74638529d52ce6001e4f48
SimonAtelier <simon_atelier@web.de>
2021-04-21 09:17:00 +0200

    Spawn item view.

---
+++ MiniGameAPI/src/view/SpawnItemView.java
@@ -0,0 +1,7 @@
+package view;
+
+public interface SpawnItemView {
+
+	public void display(SpawnItemViewModel viewModel);
+	
+}
---
+++ MiniGameAPI/src/view/SpawnItemViewModel.java
@@ -0,0 +1,29 @@
+package view;
+
+public interface SpawnItemViewModel {
+	
+	int getAmount();
+	
+	void setAmount(int amount);
+	
+	double getX();
+	
+	void setX(double x);
+	
+	double getY();
+	
+	void setY(double y);
+	
+	double getZ();
+	
+	void setZ(double z);
+	
+	String getWorld();
+	
+	void setWorld(String world);
+	
+	String getItemType();
+	
+	void setItemType(String itemType);
+	
+}
