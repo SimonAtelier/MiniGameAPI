78b2d5090d4aebb16e193a8d57ccaa8f7661fec1
SimonAtelier <simon_atelier@web.de>
2021-04-23 11:25:30 +0200

    Changed the design of the message builder class to ensure first color then format. No multiple color or format codes. Also added test class.

--- MiniGameAPI/src/view/MessageBuilder.java
+++ MiniGameAPI/src/view/MessageBuilder.java
@@ -2,133 +2,185 @@ package view;
 
 public class MessageBuilder {
 
-	StringBuffer buffer;
-
+	private String last;
+	private StringBuffer buffer;
+	
 	private MessageBuilder() {
 		buffer = new StringBuffer();
 	}
 	
+	private MessageBuilder(StringBuffer buffer) {
+		this.buffer = buffer;
+	}
+	
 	public static MessageBuilder builder() {
 		return new MessageBuilder();
-	}
-
-	public String build() {
-		return buffer.toString();
 	}
 	
 	public MessageBuilder text(String text) {
 		buffer.append(text);
 		return this;
 	}
-
-	public MessageBuilder black() {
-		buffer.append(MessageCodes.BLACK);
-		return this;
-	}
-
-	public MessageBuilder darkBlue() {
-		buffer.append(MessageCodes.DARK_BLUE);
-		return this;
-	}
-
-	public MessageBuilder darkGreen() {
-		buffer.append(MessageCodes.DARK_GREEN);
-		return this;
-	}
-
-	public MessageBuilder darkAqua() {
-		buffer.append(MessageCodes.DARK_AQUA);
-		return this;
-	}
-
-	public MessageBuilder darkRed() {
-		buffer.append(MessageCodes.DARK_RED);
-		return this;
-	}
-
-	public MessageBuilder darkPurple() {
-		buffer.append(MessageCodes.DARK_PURPLE);
-		return this;
-	}
-
-	public MessageBuilder gold() {
-		buffer.append(MessageCodes.GOLD);
-		return this;
-	}
-
-	public MessageBuilder gray() {
-		buffer.append(MessageCodes.GRAY);
-		return this;
-	}
-
-	public MessageBuilder darkGray() {
-		buffer.append(MessageCodes.DARK_GRAY);
-		return this;
-	}
-
-	public MessageBuilder blue() {
-		buffer.append(MessageCodes.BLUE);
-		return this;
-	}
-
-	public MessageBuilder green() {
-		buffer.append(MessageCodes.GREEN);
-		return this;
-	}
-
-	public MessageBuilder aqua() {
-		buffer.append(MessageCodes.AQUA);
-		return this;
-	}
-
-	public MessageBuilder red() {
-		buffer.append(MessageCodes.RED);
-		return this;
-	}
-
-	public MessageBuilder lightPurple() {
-		buffer.append(MessageCodes.LIGHT_PURPLE);
-		return this;
-	}
-
-	public MessageBuilder yellow() {
-		buffer.append(MessageCodes.YELLOW);
-		return this;
-	}
-
-	public MessageBuilder white() {
-		buffer.append(MessageCodes.WHITE);
-		return this;
-	}
-
-	public MessageBuilder magic() {
-		buffer.append(MessageCodes.MAGIC);
-		return this;
-	}
-
-	public MessageBuilder bold() {
-		buffer.append(MessageCodes.BOLD);
-		return this;
-	}
-	
-	public MessageBuilder strikeThrough() {
-		buffer.append(MessageCodes.STRIKE_THROUGH);
-		return this;
-	}
-	
-	public MessageBuilder underline() {
-		buffer.append(MessageCodes.UNDERLINE);
-		return this;
-	}
-	
-	public MessageBuilder italic() {
-		buffer.append(MessageCodes.ITALIC);
-		return this;
-	}
 	
 	public MessageBuilder reset() {
+		if (MessageCodes.RESET.equals(last))
+			return this;
 		buffer.append(MessageCodes.RESET);
+		last = MessageCodes.RESET;
 		return this;
 	}
+		
+	public String build() {
+		return buffer.toString();
+	}
+	
+	public TextBuilder darkRed() {
+		buffer.append(MessageCodes.DARK_RED);
+		return new TextBuilder();
+	}
+	
+	public TextBuilder red() {
+		buffer.append(MessageCodes.RED);
+		return new TextBuilder();
+	}
+	
+	public TextBuilder gold() {
+		buffer.append(MessageCodes.GOLD);
+		return new TextBuilder();
+	}
+	
+	public TextBuilder yellow() {
+		buffer.append(MessageCodes.YELLOW);
+		return new TextBuilder();
+	}
+	
+	public TextBuilder darkGreen() {
+		buffer.append(MessageCodes.DARK_GREEN);
+		return new TextBuilder();
+	}
+	
+	public TextBuilder green() {
+		buffer.append(MessageCodes.GREEN);
+		return new TextBuilder();
+	}
+	
+	public TextBuilder aqua() {
+		buffer.append(MessageCodes.AQUA);
+		return new TextBuilder();
+	}
+	
+	public TextBuilder darkAqua() {
+		buffer.append(MessageCodes.DARK_AQUA);
+		return new TextBuilder();
+	}
+	
+	public TextBuilder darkBlue() {
+		buffer.append(MessageCodes.DARK_BLUE);
+		return new TextBuilder();
+	}
+	
+	public TextBuilder blue() {
+		buffer.append(MessageCodes.BLUE);
+		return new TextBuilder();
+	}
+	
+	public TextBuilder lightPurple() {
+		buffer.append(MessageCodes.LIGHT_PURPLE);
+		return new TextBuilder();
+	}
+	
+	public TextBuilder white() {
+		buffer.append(MessageCodes.WHITE);
+		return new TextBuilder();
+	}
+	
+	public TextBuilder gray() {
+		buffer.append(MessageCodes.GRAY);
+		return new TextBuilder();
+	}
+	
+	public TextBuilder darkGray() {
+		buffer.append(MessageCodes.DARK_GRAY);
+		return new TextBuilder();
+	}
+	
+	public TextBuilder black() {
+		buffer.append(MessageCodes.BLACK);
+		return new TextBuilder();
+	}
+	
+	public class TextBuilder {
+	
+		private String last;
+		
+		private TextBuilder() {
+			last = null;
+		}
+		
+		private void updateLast(String last) {
+			this.last = last;
+		}
+		
+		private void append(String str) {
+			buffer.append(str);
+			updateLast(str);
+		}
+		
+		public TextBuilder magic() {
+			if (MessageCodes.MAGIC.equals(last))
+				return this;
+			
+			append(MessageCodes.MAGIC);
+			return this;
+		}
+		
+		public TextBuilder bold() {
+			if (MessageCodes.BOLD.equals(last))
+				return this;
+			
+			append(MessageCodes.BOLD);
+			return this;
+		}
+		
+		public TextBuilder strikeThrough() {
+			append(MessageCodes.STRIKE_THROUGH);
+			return this;
+		}
+		
+		public TextBuilder underline() {
+			if (MessageCodes.UNDERLINE.equals(last))
+				return this;
+			
+			append(MessageCodes.UNDERLINE);
+			return this;
+		}
+		
+		public TextBuilder italic() {
+			if (MessageCodes.ITALIC.equals(last))
+				return this;
+			
+			append(MessageCodes.ITALIC);
+			return this;
+		}
+		
+		public TextBuilder reset() {
+			if (MessageCodes.RESET.equals(last))
+				return this;
+			
+			append(MessageCodes.RESET);
+			return this;
+		}
+		
+		public MessageBuilder text(String text) {
+			append(text);
+			return new MessageBuilder(buffer);
+		}
+		
+		public String build() {
+			return buffer.toString();
+		}
+		
+	}
+	
 	
 }
---
+++ MiniGameAPI/test/tests/MessageBuilderTest.java
@@ -0,0 +1,278 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import view.MessageBuilder;
+import view.MessageBuilder.TextBuilder;
+import view.MessageCodes;
+
+public class MessageBuilderTest {
+
+	@Test
+	public void plainText() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().text(randomString).build();
+		assertEquals(randomString, message);
+	}
+	
+	@Test
+	public void combinedPlainText() {
+		String randomString0 = TestUtil.createRandomString();
+		String randomString1 = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().text(randomString0).text(randomString1).build();
+		assertEquals(randomString0 + randomString1, message);
+	}
+	
+	@Test
+	public void darkRedText() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().darkRed().text(randomString).build();
+		assertEquals(MessageCodes.DARK_RED + randomString, message);
+	}
+	
+	@Test
+	public void redText() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().red().text(randomString).build();
+		assertEquals(MessageCodes.RED + randomString, message);
+	}
+	
+	@Test
+	public void goldText() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().gold().text(randomString).build();
+		assertEquals(MessageCodes.GOLD + randomString, message);
+	}
+	
+	@Test
+	public void yellowText() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().yellow().text(randomString).build();
+		assertEquals(MessageCodes.YELLOW + randomString, message);
+	}
+	
+	@Test
+	public void darkGreenText() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().darkGreen().text(randomString).build();
+		assertEquals(MessageCodes.DARK_GREEN + randomString, message);
+	}
+	
+	@Test
+	public void greenText() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().green().text(randomString).build();
+		assertEquals(MessageCodes.GREEN + randomString, message);
+	}
+	
+	@Test
+	public void aquaText() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().aqua().text(randomString).build();
+		assertEquals(MessageCodes.AQUA + randomString, message);
+	}
+	
+	@Test
+	public void darkAquaText() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().darkAqua().text(randomString).build();
+		assertEquals(MessageCodes.DARK_AQUA + randomString, message);
+	}
+	
+	@Test
+	public void darkBlueText() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().darkBlue().text(randomString).build();
+		assertEquals(MessageCodes.DARK_BLUE + randomString, message);
+	}
+	
+	@Test
+	public void blueText() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().blue().text(randomString).build();
+		assertEquals(MessageCodes.BLUE + randomString, message);
+	}
+	
+	@Test
+	public void lightPurpleText() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().lightPurple().text(randomString).build();
+		assertEquals(MessageCodes.LIGHT_PURPLE + randomString, message);
+	}
+	
+	@Test
+	public void whiteText() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().white().text(randomString).build();
+		assertEquals(MessageCodes.WHITE + randomString, message);
+	}
+	
+	@Test
+	public void grayText() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().gray().text(randomString).build();
+		assertEquals(MessageCodes.GRAY + randomString, message);
+	}
+	
+	@Test
+	public void blackText() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().black().text(randomString).build();
+		assertEquals(MessageCodes.BLACK + randomString, message);
+	}
+	
+	@Test
+	public void darkGray() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().darkGray().text(randomString).build();
+		assertEquals(MessageCodes.DARK_GRAY + randomString, message);
+	}
+	
+	@Test
+	public void boldDarkRed() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().darkRed().bold().text(randomString).build();
+		assertEquals(MessageCodes.DARK_RED + MessageCodes.BOLD + randomString, message);
+	}
+	
+	@Test
+	public void boldRandomTimesInARowAppendsOnlyOne() {
+		int random = (int) (Math.random() * 100);
+		String randomString = TestUtil.createRandomString();
+		
+		MessageBuilder builder = MessageBuilder.builder();
+		TextBuilder textBuilder = builder.red();
+		for (int i = 0; i < random; i++) {
+			textBuilder = textBuilder.bold();
+		}
+		
+		String message = textBuilder.text(randomString).build();
+		
+		assertEquals(MessageCodes.RED + MessageCodes.BOLD + randomString, message);
+	}
+	
+	@Test
+	public void italicDarkRed() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().darkRed().italic().text(randomString).build();
+		assertEquals(MessageCodes.DARK_RED + MessageCodes.ITALIC + randomString, message);
+	}
+	
+	@Test
+	public void italicRandomTimesInARowAppendsOnlyOne() {
+		int random = (int) (Math.random() * 100);
+		String randomString = TestUtil.createRandomString();
+		
+		MessageBuilder builder = MessageBuilder.builder();
+		TextBuilder textBuilder = builder.red();
+		for (int i = 0; i < random; i++) {
+			textBuilder = textBuilder.italic();
+		}
+		
+		String message = textBuilder.text(randomString).build();
+		
+		assertEquals(MessageCodes.RED + MessageCodes.ITALIC + randomString, message);
+	}
+	
+	@Test
+	public void underlineDarkRed() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().darkRed().underline().text(randomString).build();
+		assertEquals(MessageCodes.DARK_RED + MessageCodes.UNDERLINE + randomString, message);
+	}
+	
+	@Test
+	public void underlineRandomTimesInARowAppendsOnlyOne() {
+		int random = (int) (Math.random() * 100);
+		String randomString = TestUtil.createRandomString();
+		
+		MessageBuilder builder = MessageBuilder.builder();
+		TextBuilder textBuilder = builder.red();
+		for (int i = 0; i < random; i++) {
+			textBuilder = textBuilder.underline();
+		}
+		
+		String message = textBuilder.text(randomString).build();
+		
+		assertEquals(MessageCodes.RED + MessageCodes.UNDERLINE + randomString, message);
+	}
+	
+	@Test
+	public void magicDarkRed() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().darkRed().magic().text(randomString).build();
+		assertEquals(MessageCodes.DARK_RED + MessageCodes.MAGIC + randomString, message);
+	}
+	
+	@Test
+	public void magicRandomTimesInARowAppendsOnlyOne() {
+		int random = (int) (Math.random() * 100);
+		String randomString = TestUtil.createRandomString();
+		
+		MessageBuilder builder = MessageBuilder.builder();
+		TextBuilder textBuilder = builder.red();
+		for (int i = 0; i < random; i++) {
+			textBuilder = textBuilder.magic();
+		}
+		
+		String message = textBuilder.text(randomString).build();
+		
+		assertEquals(MessageCodes.RED + MessageCodes.MAGIC + randomString, message);
+	}
+	
+	@Test
+	public void strikeThroughDarkRed() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().darkRed().strikeThrough().text(randomString).build();
+		assertEquals(MessageCodes.DARK_RED + MessageCodes.STRIKE_THROUGH + randomString, message);
+	}
+	
+	@Test
+	public void resetAfterColor() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().darkAqua().reset().text(randomString).build();
+		assertEquals(MessageCodes.DARK_AQUA + MessageCodes.RESET + randomString, message);
+	}
+	
+	@Test
+	public void resetRandomTimesInARowAppendsOnlyOne() {
+		int random = (int) (Math.random() * 100);
+		String randomString = TestUtil.createRandomString();
+		
+		MessageBuilder builder = MessageBuilder.builder();
+		TextBuilder textBuilder = builder.red();
+		for (int i = 0; i < random; i++) {
+			textBuilder = textBuilder.reset();
+		}
+		
+		String message = textBuilder.text(randomString).build();
+		
+		assertEquals(MessageCodes.RED + MessageCodes.RESET + randomString, message);
+	}
+	
+	@Test
+	public void resetBeforeColor() {
+		String randomString = TestUtil.createRandomString();
+		String message = MessageBuilder.builder().reset().darkAqua().text(randomString).build();
+		assertEquals(MessageCodes.RESET + MessageCodes.DARK_AQUA + randomString, message);
+	}
+	
+	@Test
+	public void resetRandomTimesInARowBeforeColorAppendsOnlyOne() {
+		int random = (int) (Math.random() * 100);
+		String randomString = TestUtil.createRandomString();
+		
+		MessageBuilder builder = MessageBuilder.builder().reset();
+
+		for (int i = 0; i < random; i++) {
+			builder = builder.reset();
+		}
+		
+		String message = builder.red().text(randomString).build();
+		
+		assertEquals(MessageCodes.RESET + MessageCodes.RED + randomString, message);
+	}
+	
+}
